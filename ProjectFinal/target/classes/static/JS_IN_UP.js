3c7ce2106f54413b61d4e2d65c478a2ae6af128c
thanhtuyen-hub <77259460+thanhtuyen-hub@users.noreply.github.com>
2022-12-16 12:51:59 +0700

    del

--- ProjectFinal/target/classes/static/JS_IN_UP.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-btn');
-let navbar1 = document.querySelector('.header .navbar-1');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar1.classList.toggle('active');
-}
